b11c900fb257439ac64b19e2c00108b1bc4188d8
TheHarshSro <theharshsro@gmail.com>
2026-04-09 22:14:00 +0530

    added first func

--- Projects/C-ext/main.c
+++ Projects/C-ext/main.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main(int argc, char *argv[]){
+    FILE *fptr;
+    if (argc > 1) {
+        if (argv[1] == 0) {
+             fptr = fopen(argv[1], "w");
+                if (fptr) {
+                    fclose(fptr);
+            }
+        }
+        else {
+            fptr = fopen(argv[1], "w");
+            fprintf(fptr, "This file already exists");
+            fclose(fptr);
+        }
+    }
+    return 0;
+}
