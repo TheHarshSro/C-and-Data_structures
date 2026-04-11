71a07a0acee23208ad14242c401184598695d135
TheHarshSro <theharshsro@gmail.com>
2026-04-11 11:31:59 +0530

    small change

--- Projects/C-ext/main.c
+++ Projects/C-ext/main.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]){
-    FILE *fptr;
-    if (argc > 1) {
-        if (argv[1] == 0) {
-             fptr = fopen(argv[1], "w");
-                if (fptr) {
-                    fclose(fptr);
-            }
-        }
-/*        else {
-            fptr = fopen(argv[1], "w");
-            fprintf(fptr, "This file already exists");
-            printf("This already exists");
+int main(int argc,char *argv[]) {
+    if (argc < 2){
+        printf("Usage %s <filename>", argv[0]);
+        return 1;
+    }
+
+    FILE *fptr = fopen(argv[1], "r");
+
+    if(fptr){
+        printf("The file '%s' already exists", argv[1]);
+        fclose(fptr);
+    } else {
+        fptr = fopen(argv[1], "w");
+        if(fptr) {
+            printf("%s created.", argv[1]);
             fclose(fptr);
+        } else {
+            printf("Error creating file.");
         }
-        */
     }
     return 0;
 }
